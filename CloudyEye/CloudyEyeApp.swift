96df05475f98aa8fe9b74bc454aa61f0691d491f
Youssef DOnadeo <you.donadeo@icloud.com>
2022-09-30 19:16:30 +0200

    Create app base structure

---
+++ CloudyEye/CloudyEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudyEyeApp.swift
+//  CloudyEye
+//
+//  Created by Youssef Donadeo on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CloudyEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloudyEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CloudyEye
+//
+//  Created by Youssef Donadeo on 30/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
